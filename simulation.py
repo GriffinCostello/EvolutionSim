01017dd693ce8c72177cc1f8c5ed5f5ac21ded16
Griffin <gmcostello20@gmail.com>
2025-12-11 15:16:10 -0800

    Created Sim Class for refactoring purposes

--- simulation.py
+++ simulation.py
@@ -4,10 +4,29 @@ import random
 import math
 import sys
 
-worldSize = 1000
-organismList = []
-childCounter = 0
-world = None
+
+class Simulation:
+    def __init__(self, worldsize):
+        self.env = simpy.Environment()
+        self.worldSize = worldsize
+        self.world = self.initMap()
+        self.organismList = []
+        self.childCounter = 0
+
+    def initMap(self):
+        map = np.zeros((self.worldSize, self.worldSize), dtype=int)
+        numPoints = (self.worldSize * self.worldSize) // 800  # approx 1 every 800 spaces
+
+        # pick random coordinates
+        xs = np.random.randint(0, self.worldSize, numPoints)
+        ys = np.random.randint(0, self.worldSize, numPoints)
+
+        map[xs, ys] = 1
+
+        return map
+
+    def run(self, ticks):
+        self.env.run(until=ticks)
 
 class Traits:
     def __init__(self, detectionRadius, speed, energy, slowDownAge, reproductionAge, matingCallRadius):
@@ -23,14 +42,12 @@ class Traits:
 
 
 class Organism:
-    def __init__(self, name, species, age, posX , posY, traits: Traits, env, world):
+    def __init__(self, name, species, age, posX , posY, traits: Traits, sim):
         self.name = name
         self.species = species
         self.age = age
         self.posX = posX
         self.posY = posY
-        self.env = env
-        self.world = world
 
         self.detectionRadius = traits.detectionRadius
         self.speed = traits.speed
@@ -41,33 +58,37 @@ class Organism:
         self.reproductionAge = traits.reproductionAge
         self.matingCallRadius = traits.matingCallRadius
 
-        self.live = env.process(self.live())
+        self.sim = sim
+        self.env = sim.env
+        self.world = sim.world
+
+        self.live = self.env.process(self.live())
 
 
     def tick(self):
         self.age += 1
         if self.age == self.slowDownAge:
             self.speed = max(self.speed - 1, 1)
-            self.slowDownAge += 10
+            self.slowDownAge += (self.slowDownAge // 2)
     
         self.energy = max(self.energy - self.energyConsumption, 0)  # Decrease energy each tick
 
 
     def scanForFood(self, world):
-        x_min = max(self.posX - self.detectionRadius, 0)
-        x_max = min(self.posX + self.detectionRadius + 1, worldSize)
-        y_min = max(self.posY - self.detectionRadius, 0)
-        y_max = min(self.posY + self.detectionRadius + 1, worldSize)
+        xMin = max(self.posX - self.detectionRadius, 0)
+        xMax = min(self.posX + self.detectionRadius + 1, self.sim.worldSize)
+        yMin = max(self.posY - self.detectionRadius, 0)
+        yMax = min(self.posY + self.detectionRadius + 1, self.sim.worldSize)
 
         # Slice region around the organism
-        area = world[x_min:x_max, y_min:y_max]
+        area = world[xMin:xMax, yMin:yMax]
         # Find actual coordinates of food
         foodPositions = np.argwhere(area == 1) 
         if len(foodPositions) == 0:
             return None  # No food found
 
         # Convert to world coordinates
-        foodGlobal = [(x_min + fx, y_min + fy) for fx, fy in foodPositions]
+        foodGlobal = [(xMin + foodX, yMin + foodY) for foodX, foodY in foodPositions]
 
         # Compute closest food by distance
         closest = min(foodGlobal, key=lambda pos: (pos[0]-self.posX)**2 + (pos[1]-self.posY)**2)
@@ -75,36 +96,36 @@ class Organism:
 
 
     def moveTowards(self, target):
-        target_x, target_y = target
-        if self.posX < target_x:
-            if(self.posX + self.speed > target_x): #if this step would overshoot
-                self.posX = target_x               #then just go to target
+        targetX, targetY = target
+        if self.posX < targetX:
+            if(self.posX + self.speed > targetX): #if this step would overshoot
+                self.posX = targetX               #then just go to target
             else:
                 self.posX += self.speed
-        elif self.posX > target_x:
-            if(self.posX - self.speed < target_x):
-                self.posX = target_x
+        elif self.posX > targetX:
+            if(self.posX - self.speed < targetX):
+                self.posX = targetX
             else:
                 self.posX -= self.speed
 
-        if self.posY < target_y:
-            if(self.posY + self.speed > target_y):
-                self.posY = target_y
+        if self.posY < targetY:
+            if(self.posY + self.speed > targetY):
+                self.posY = targetY
             else:
                 self.posY += self.speed
-        elif self.posY > target_y:
-            if(self.posY - self.speed < target_y):
-                self.posY = target_y
+        elif self.posY > targetY:
+            if(self.posY - self.speed < targetY):
+                self.posY = targetY
             else:
                 self.posY -= self.speed
 
         self.energy = max(self.energy - self.energyConsumption, 0)
 
     def eatFood(self, world, foodPos):
-        food_x, food_y = foodPos
-        if world[food_x, food_y] == 1:
-            world[food_x, food_y] = 0  # Remove food from the world
-            print(f"{self.name} ate food at ({food_x}, {food_y})")
+        foodX, foodY = foodPos
+        if world[foodX, foodY] == 1:
+            world[foodX, foodY] = 0  # Remove food from the world
+            print(f"{self.name} ate food at ({foodX}, {foodY})")
         else:
             print(f"{self.name} can't find food to eat at this position.")
 
@@ -113,7 +134,7 @@ class Organism:
     def matingCall(self):
         print(f"{self.name} is making a mating call!")
 
-        for i in organismList:
+        for i in self.sim.organismList:
             if i == self:
                 continue
             if i.age < i.reproductionAge:
@@ -130,7 +151,8 @@ class Organism:
 
     def decideNextAction(self):
         if self.energy > self.energyCapacity * 0.7:
-            return "Mate"
+            if(self.age >= self.reproductionAge):
+                return "Mate"
 
         closestFood = self.scanForFood(self.world)
         if closestFood:
@@ -145,7 +167,11 @@ class Organism:
             self.tick()
             if self.energy <= 0:
                 print(f"{self.name} has run out of energy and died at age {self.age}.")
-                organismList.remove(self)
+                
+                if len(self.sim.organismList) == 1:
+                    print("All organisms have died. Ending simulation.")
+                    sys.exit()
+                self.sim.organismList.remove(self)
                 break
 
             nextAction = self.decideNextAction()
@@ -163,8 +189,8 @@ class Organism:
 
                 case "Wander":
                     dx, dy = random.choice([(self.speed,0),(-self.speed,0),(0,self.speed),(0,-self.speed)])
-                    self.posX = (self.posX + dx) % worldSize
-                    self.posY = (self.posY + dy) % worldSize
+                    self.posX = (self.posX + dx) % self.sim.worldSize
+                    self.posY = (self.posY + dy) % self.sim.worldSize
                     self.status = "Wandering"
             
             yield self.env.timeout(1)
@@ -174,13 +200,12 @@ def mate(org1, org2):
         print(f"{org1.name} and {org2.name} have mated at distance {distance}!")
         org1.energy = max(org1.energy - 50, 0)
         org2.energy = max(org2.energy - 50, 0)
-        reproduce(org1, org2, org1.env, org1.world)
+        reproduce(org1, org2)
 
 
-def reproduce(parent1, parent2, env, world):
-    global childCounter
-    childCounter += 1
-    childName = "Gen2_" + str(childCounter)
+def reproduce(parent1, parent2):
+    parent1.sim.childCounter += 1
+    childName = "Gen2_" + str(parent1.sim.childCounter)
     childTraits = Traits(
         detectionRadius = (parent1.detectionRadius + parent2.detectionRadius) // 2 + 2*random.choice([-1, 1]),
         speed = (parent1.speed + parent2.speed) //2 + 1*random.choice([-1, 1]),
@@ -197,18 +222,16 @@ def reproduce(parent1, parent2, env, world):
         posX = parent1.posX, 
         posY = parent1.posY, 
         traits = childTraits,
-        env = env, 
-        world = world
+        sim = parent1.sim
     )
-    organismList.append(childName)
+    parent1.sim.organismList.append(childName)
     parent1.energy = max(parent1.energy - 30, 0)
     parent2.energy = max(parent2.energy - 30, 0)
     print(f"{parent1.name} and {parent2.name} have reproduced to create {childName.name}!")
         
 
 def main():
-    env = simpy.Environment()
-    world = initMap()
+    sim = Simulation(worldsize=1000)
     genOneTraits = Traits(
         detectionRadius = 20,
         speed = 5,
@@ -218,33 +241,19 @@ def main():
         matingCallRadius = 200
     )
 
-    for i in range(50):
+    for i in range(100):
         org = Organism(
             name = "Gen1_" + str(i), 
             species = "Lion", 
             age = random.randint(1, 10), 
-            posX = np.random.randint(0, worldSize), 
-            posY = np.random.randint(0, worldSize), 
+            posX = np.random.randint(0, sim.worldSize), 
+            posY = np.random.randint(0, sim.worldSize), 
             traits = genOneTraits,
-            env = env, 
-            world = world
+            sim = sim
         )
-        organismList.append(org)
+        org.sim.organismList.append(org)
 
-    env.run(until=50000)
-
-
-def initMap():
-    map = np.zeros((worldSize, worldSize), dtype=int)
-    num_points = (worldSize * worldSize) // 800  # approx 1 every 800 spaces
-
-    # pick random coordinates
-    xs = np.random.randint(0, worldSize, num_points)
-    ys = np.random.randint(0, worldSize, num_points)
-
-    map[xs, ys] = 1
-
-    return map
+    sim.run(ticks = 50000)
 
 if __name__ == "__main__":
     main()
