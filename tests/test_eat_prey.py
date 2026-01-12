131565c195a35ac2fba28373b27ad364701e27a9
Griffin <gmcostello20@gmail.com>
2026-01-12 15:28:40 -0800

    test: added test case for herbivore fleeing from carnivore

--- tests/test_eat_prey.py
+++ tests/test_eat_prey.py
@@ -111,3 +111,51 @@ def test_carnivore_no_prey():
     assert carnivore1 in simulation.organismList, "Carnivore was incorrectly removed from the simulation."
     assert carnivore2 in simulation.organismList, "Carnivore was incorrectly removed from the simulation."
     assert len(simulation.organismList) == 2, "There should be two organisms left in the simulation."
+
+
+def test_herbivore_flee():
+    simulation = Simulation(worldsize=10)
+    herbivore = Organism(
+        name = "Herbivore_Test", 
+        species = "Herbivore",
+        age = 5,
+        energy = 100,
+        position = Position(x=1, y=1),
+        traits = HerbivoreTraits(
+            detectionRadius = 5,
+            speed = 1,
+            energyCapacity = 250,
+            birthEnergy = 80,
+            slowDownAge = 30,
+            reproductionAge = 20,
+            matingCallRadius = 200,
+            digestionTime = 8,
+            generation = 1
+        ),
+        simulation = simulation
+    )
+    herbivore.simulation.organismList.append(herbivore)
+
+    carnivore = Organism(
+        name = "Carnivore_Test", 
+        species = "Carnivore",
+        age = 5,
+        energy = 90,
+        position = Position(x=2, y=2),
+        traits = CarnivoreTraits(
+            huntingRadius = 5,
+            speed = 0,
+            energyCapacity = 250,
+            birthEnergy = 80,
+            slowDownAge = 30,
+            reproductionAge = 20,
+            matingCallRadius = 200,
+            digestionTime = 8,
+            generation = 1
+        ),
+        simulation = simulation
+    )
+    carnivore.simulation.organismList.append(carnivore)
+
+    simulation.run(ticks = 1)
+    assert herbivore.position.asTuple() == (0, 0), "Herbivore did not flee from the carnivore."
